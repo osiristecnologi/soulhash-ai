8d0a8ced80b25ae557e286ecd7853f37b49d6eae
Osiris technolog <107819115+osiristecnologi@users.noreply.github.com>
2026-05-01 14:14:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function gerarAlma() {
+  const nomes = ["Aether", "Nyx", "Vortex", "Orion", "Eclipse"];
+  const tipos = ["Arcano", "Caótico", "Luminar", "Sombrio"];
 
+  return {
+    nome: nomes[Math.floor(Math.random() * nomes.length)],
+    tipo: tipos[Math.floor(Math.random() * tipos.length)],
+    nivel: Math.floor(Math.random() * 100),
+    energia: Math.floor(Math.random() * 1000)
+  };
+}
